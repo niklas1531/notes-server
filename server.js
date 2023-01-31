6c2d3e27769e89402aab9e41b1ec0c66760bdc71
Niklas Minth <niklas.minth@gmail.com>
2023-01-31 16:02:12 +0100

    added create note route

--- server.js
+++ server.js
@@ -64,7 +64,16 @@ app.get('/notes/:userEmail', async (req, res) => {
         console.log(error)
     }
 })
-
+app.post('/notes', async (req, res) => {
+    const { user_email, title, progress, date } = req.body
+    const id = uuidv4()
+    try {
+        const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1,$2,$3,$4,$5)`, [id, user_email, title, progress, date])
+        res.json(newToDo)
+    } catch (error) {
+        console.log(error)
+    }
+})
 app.put('/notes/:id', async (req, res) => {
     const { id } = req.params
     const { user_email, title, progress, date } = req.body
