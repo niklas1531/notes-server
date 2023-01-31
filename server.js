f6cabc968f97a22c1500e1d0ce45036bd5cd11b2
Niklas Minth <niklas.minth@gmail.com>
2023-01-31 16:06:00 +0100

    fixed bug in POST route

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/notes', async (req, res) => {
     const { user_email, title, progress, date } = req.body
     const id = uuidv4()
     try {
-        const newToDo = await pool.query(`INSERT INTO todos(id, user_email, title, progress, date) VALUES($1,$2,$3,$4,$5)`, [id, user_email, title, progress, date])
+        const newToDo = await pool.query(`INSERT INTO notes(id, user_email, title, progress, date) VALUES($1,$2,$3,$4,$5)`, [id, user_email, title, progress, date])
         res.json(newToDo)
     } catch (error) {
         console.log(error)
